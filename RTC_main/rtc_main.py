045213c0e77ba6b32a1d83e2330334d8d995a9a2
VU-eduardschneider <e.schneider@student.vu.nl>
2024-09-27 11:17:46 +0200

    pattern-matching callback rtc_main.py
    Adjusted code for pattern-matching information between connector dropdown and its associated fields, and the data that will be retrieved

--- RTC_main/rtc_main.py
+++ RTC_main/rtc_main.py
@@ -1,82 +1,123 @@
 import dash
-from dash import html, dcc, Input, Output, State
+from dash import *
 import dash_bootstrap_components as dbc
 import pandas as pd
 import time
+import json
 
 # Initialize Dash app
-app = dash.Dash(__name__, external_stylesheets= [dbc.themes.BOOTSTRAP], suppress_callback_exceptions= True)
+app = dash.Dash(
+    __name__,
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    suppress_callback_exceptions=True,
+)
 
 # Layout of the app
-app.layout = dbc.Container([
+app.layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.Div(
+                        [  # Sidebar
+                            html.H2("Sidebar"),
+                            html.Hr(),
+                            dcc.Dropdown(  # Connector dropdown menu
+                                id="connector-dropdown",
+                                options=[
+                                    {"label": "IP", "value": "IP_connector"},
+                                    {"label": "TCP", "value": "TCP_connector"},
+                                ],
+                                value="IP_connector",
+                            ),  # Default option on launch
+                            html.Div(
+                                id="input-fields", children=[]
+                            ),  # Dynamic input field(s) for each dropdown option
+                            html.Button("Connect", id="connector-button", n_clicks=0),
+                        ],
+                        style={
+                            "padding": "20px",
+                            "backgroundColor": "#f8f9fa",
+                            "height": "100vh",
+                        },
+                    ),
+                    width=2,
+                ),
+                dbc.Col(
+                    html.Div(
+                        [  # Main content area
+                            html.H2("Main content"),
+                            html.Hr(),
+                            html.Div(id="output", children=[]),
+                        ],
+                        style={"padding": "20px"},
+                    ),
+                    width=10,
+                ),
+            ]
+        ),
+    	dcc.Interval(id="data_interval", interval=1000, disabled=True, )
+    ],
+    fluid=True,
+)
 
-	dbc.Row([
-		dbc.Col(html.Div([ # Sidebar
-			html.H2('Sidebar'),
-			html.Hr(),
-			dcc.Dropdown( # Dropdown menu
-				id= 'connector-dropdown',
-				options= [
-				{'label': 'IP', 'value': 'IP_connector'},
-				{'label' : 'TCP', 'value' : 'TCP_connector'}
-				],
-				value= 'IP_connector'), # Default option on launch
-
-			html.Div(id= 'input-fields', children= []), # Dynamic input field
-			html.Button('Connect', id= 'connector-button', n_clicks= 0),
-			], style={'padding' : '20px', 'backgroundColor' : '#f8f9fa', 'height': '100vh'}), width= 2),
-
-
-		dbc.Col(html.Div([ # Main content area
-			html.H2('Main content'),
-			html.Hr(),
-			html.Div(id= 'output', children=[]),
-			], style= {'padding': '20px'}), width = 10
-		)
-	])
-	], fluid= True)
 
 # Callback to dynamically change input fields based on source
 @app.callback(
-	Output(component_id= 'input-fields', component_property= 'children'),
-	Input(component_id= 'connector-dropdown', component_property= 'value')
-	)
-
+    Output(component_id="input-fields", component_property="children"),
+    Input(component_id="connector-dropdown", component_property="value"),
+)
 def update_input_fields(connector_type):
+    if connector_type == "IP_connector":
+        return html.Div(
+            [
+                dcc.Input(
+                    id={"index": "IP_input", "type": "connection_input"},
+                    type="text",
+                    placeholder="Enter IP address",
+                )
+            ]
+        )
 
-	if connector_type == 'IP_connector':
-		return html.Div([
-			dcc.Input(id= 'IP_input', type= 'text', placeholder= 'Enter IP address')
-		])
+    elif connector_type == "TCP_connector":
+        return html.Div(
+            [
+                dcc.Input(
+                    id={"index": "TCP1_input", "type": "connection_input"},
+                    type="text",
+                    placeholder="Enter TCP1 address",
+                ),
+                dcc.Input(
+                    id={"index": "TCP2_input", "type": "connection_input"},
+                    type="text",
+                    placeholder="Enter TCP2 address",
+                ),
+            ]
+        )
+    # Add mor elifs for new connection types here
+    else:
+        return html.Div()
 
-	elif connector_type == 'TCP_connector':
-		return html.Div([
-				dcc.Input(id= 'TCP1_input', type= 'text', placeholder= 'Enter TCP1 address'),
-				dcc.Input(id= 'TCP2_input', type= 'text', placeholder= 'Enter TCP2 address')
-		])
-	else:
-		return html.Div()
 
 # Callback to retrieve data and upate the main content
 @app.callback(
-	Output(component_id= 'output', component_property= 'children'),
-	Input(component_id= 'connector-button', component_property= 'n_clicks'),
-	State(component_id= 'connector-dropdown', component_property= 'value'),
-	State(component_id= 'IP_input', component_property= 'value'),
-	State(component_id= 'TCP1_input', component_property= 'value'),
-	State(component_id= 'TCP2_input', component_property= 'value'),
-	prevent_initial_call = True
+    Output(component_id="output", component_property="children"),
+    Input(component_id="connector-button", component_property="n_clicks"),
+    State(component_id="connector-dropdown", component_property="value"),
+    State({"index": ALL, "type": "connection_input"}, "id"),
+    State({"index": ALL, "type": "connection_input"}, "value"),
+    prevent_initial_call=True,
 )
+def retrieve_data(n_clicks, connector_type, custom_id, connection_input):
+    connection = ''
+    if n_clicks > 0:
+        if connector_type:
+            print(f'{connector_type.split("_")[0]} success!')
+        connection_data = {custom_id[x]['index']: str(connection_input[x]) for x in range(len(connection_input))}
+        connection = f'{connector_type.split("_")[0]} - {connection_data}'
+    return connection
 
-def retrieve_data(n_clicks, connector_type, IP_input, TCP1_input, TCP2_input):
-	if n_clicks > 0:
-		print('success!')
-		if connector_type == 'IP_connector':
-			print('IP success!')
-
-		elif connector_type == 'TCP_connector':
-			print('TCP success!')
 
 # Run app
-if __name__ == '__main__':
-	app.run_server(debug= True)
+if __name__ == "__main__":
+    app.run_server(debug=True)
