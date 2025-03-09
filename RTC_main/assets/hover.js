f6ce619e8244e8334f8800af0524f60e1edfa77d
edevard <edikschneider@protonmail.com>
2025-03-09 14:04:15 +0100

    Delete hover.js

--- RTC_main/assets/hover.js
+++
@@ -1,9 +0,0 @@
-// assets/hover.js
-document.addEventListener('DOMContentLoaded', function() {
-    const divs = document.querySelectorAll('div');
-    divs.forEach(div => {
-        if (div.id) {
-            div.setAttribute('title', div.id);
-        }
-    });
-});
